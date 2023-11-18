f57b8c8ce095209a4af0c8d7fafce62d57aaa7b3
Pavellebedev610 <104491992+Pavellebedev610@users.noreply.github.com>
2023-11-18 10:15:10 +0300

    мой тухлый проект

---
+++ automasterskaya1.api/TimeTable207.Context.Contracts/Models/BaseAuditEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTable207.Context.Contracts.Models
+{
+    public abstract class BaseAuditEntity
+    {
+        public Guid id { get; set; }
+
+        public DateTimeOffset CreateAt { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public string UpdatedBy { get; set; }= string.Empty;
+        
+        public DateTimeOffset? DeleteAt { get; set;} 
+    }
+
+}
